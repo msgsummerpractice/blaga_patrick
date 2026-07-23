092dbca67c43646a1d5d88465b9b18c44f740d29
Patrick Blaga <blaga.patrick@yahoo.com>
2026-07-23 14:58:03 +0200

    Fix spring annotations

--- springapp/src/main/java/com/example/Car.java
+++ springapp/src/main/java/com/example/Car.java
@@ -2,7 +2,8 @@ package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
+import org.springframework.stereotype.Component;
+@Component
 public class Car {
 
     private Engine engine;
