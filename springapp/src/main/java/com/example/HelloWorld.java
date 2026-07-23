703216849d3d2e633db3d8fd2a4c32dcca58ccb9
Patrick Blaga <blaga.patrick@yahoo.com>
2026-07-23 14:19:32 +0200

    Spring core exercise 1

---
+++ springapp/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    private String message;
+
+    public void setMessage(String message){
+        this.message  = message;
+    }
+
+    public void printMessage(){
+        System.out.println(this.message);
+    }
+    
+}
