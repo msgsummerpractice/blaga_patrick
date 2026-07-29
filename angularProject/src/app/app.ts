e6f9a0a1a0dac952e14335c04be0264a761d3c74
Patrick Blaga <blaga.patrick@yahoo.com>
2026-07-29 14:31:37 +0200

    Add navbar

--- angularProject/src/app/app.ts
+++ angularProject/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
-
+import { MatButton } from '@angular/material/button';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MatButton, MatToolbar, MatIcon],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('angularProject');
