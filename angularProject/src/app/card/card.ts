ef895e935102e6445a60d84722928a6a8decea37
Patrick Blaga <blaga.patrick@yahoo.com>
2026-07-30 11:45:35 +0300

    Add rxjs & signals for dog images

--- angularProject/src/app/card/card.ts
+++ angularProject/src/app/card/card.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [MatButton],
   templateUrl: './card.html',
   styleUrls: ['./card.css'],
 })
 export class Card {
   @Input() breed: string = '';
   @Input() description: string = '';
+
+  dogImage = signal<string>('');
+  private http = inject(HttpClient);
+
+  fetchDogImage() {
+    this.http
+      .get<{ message: string; status: string }>('https://dog.ceo/api/breeds/images/random')
+      .subscribe((response) => {
+        this.dogImage.set(response.message);
+      });
+  }
 }
