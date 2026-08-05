339c72e0018ae09841a653cfb7d5368bbd2b07ec
Patrick Blaga <blaga.patrick@yahoo.com>
2026-08-05 09:59:25 +0300

    Fix try 4

--- angularProject/src/app/app.routes.ts
+++ angularProject/src/app/app.routes.ts
@@ -11,5 +11,5 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./components/login-component/logion-component').then((m) => m.LogionComponent),
   },
-  { path: '**', component: HomeComponent },
+  { path: '**', redirectTo: '' },
 ];
